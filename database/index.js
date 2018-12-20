acb8b1aa5d616071a7cfc8b20691c24ada668981
Francis Distor <francis.distor@gmail.com>
2018-12-20 11:13:48 -0800

    Adding data to database correctly.

--- database/index.js
+++ database/index.js
@@ -1,28 +1,39 @@
 const mongoose = require('mongoose');
 const fs = require('fs');
+const path = require('path');
 const Schema = mongoose.Schema;
 
-mongoose.connect('mongodb://localhost/vacancy');
+mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true }, err => {
+  if (err) return console.log('Failed in connecting to MongoDB.');
+  console.log('Connected to MongoDB!');
+});
 
-const Availabilities = new Schema('availabilities', {
-  roomId: {
-    years: {
-      months: {
-        days: {
-          vacancy: Boolean,
-          price: Number,
-          location: String,
-          type: String,
-          reviewsummary: Number
+const vacancySchema = new Schema({
+  availability: {
+    roomId: {
+      location: String,
+      type: String,
+      reviewsummary: String,
+      years: {
+        months: {
+          days: {
+            vacancy: Boolean,
+            price: Number
+          }
         }
       }
     }
   }
 });
 
-fs.readFile('data.txt', (err, data) => {
-  if (err) return console.log('Error in reading file.');
+const Vacancy = mongoose.model('vacancy', vacancySchema);
+
+fs.readFile(path.resolve(__dirname, 'data.txt'), (err, data) => {
+  if (err) return console.log('Error in reading file.', err);
   const parsed = JSON.parse(data);
-  const availabilities = new Availabilities(parsed);
-  availabilities.save();
+  const availabilities = new Vacancy(parsed);
+  availabilities.save((err) => {
+    if (err) return console.log('Error in saving.', err);
+    console.log('Success in saving!');
+  });
 });
