e986572b525ffc4fa182772cc0206b28f76d0b45
Francis Distor <francis.distor@gmail.com>
2018-12-21 15:07:22 -0800

    Added Modal component to render.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './Navbar.jsx';
 import axios from 'axios';
+import Modal from './Modal.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -19,6 +20,7 @@ class App extends React.Component {
           <Navbar type={this.state.data[1].type} location={this.state.data[1].location}
             reviews={this.state.data[1].reviewnum} stars={this.state.data[1].reviewsummary}
             year={this.state.data[1][date.getFullYear() + 1]} click={() => this.toggleModal()}/>
+          <Modal show={this.state.modal}/>
         </div>
       );
     } else {
