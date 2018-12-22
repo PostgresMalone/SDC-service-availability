3803acdd0b8f07bac33bc17544894aaf7bc22c0d
Francis Distor <francis.distor@gmail.com>
2018-12-21 17:12:05 -0800

    Added CheckIn and CheckOut component to render.

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import style from '../scripts/style.css.js';
+import CheckIn from './CheckIn.jsx';
+import CheckOut from './CheckOut.jsx';
 
 const Modal = ({show, hide}) => {
   const displayModal = show ? style.showModal : style.hideModal;
@@ -7,6 +9,18 @@ const Modal = ({show, hide}) => {
     <div style={displayModal}>
       <section style={style.modalMain}>
         <button onClick={hide}>x</button>
+        <div>
+          <div>
+            <label>
+              <span>Dates</span>
+            </label>
+            </div>
+          <div>
+            <CheckIn />
+            <div>-></div>
+            <CheckOut />
+          </div>
+        </div>
       </section>
     </div>
   );
