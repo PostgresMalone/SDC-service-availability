a220ab0c44be173454ba1b33eca77cc1ab6bcdb2
Francis Distor <francis.distor@gmail.com>
2018-12-27 15:24:20 -0800

    Added two functions, one to find the day of the first of the month, one to make an array of arrays for the days of the week of the month.

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -8,7 +8,9 @@ class Modal extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      calendar: false
+      calendar: false,
+      start: 0,
+      end: 0
     };
   }
 
@@ -41,6 +43,49 @@ class Modal extends React.Component {
   showCalendar() {
     this.setState({calendar: true});
   }
+
+  findFirstOfMonth() {
+    const today = new Date;
+    let day = today.getDay();
+    let date = today.getDate();
+    while ((date - 1) % 7 !== 0 || date !== 1) {
+      date--;
+      day--;
+      day < 0 ? day = 6 : day;
+    }
+  }
+
+  buildWeeks(dates, start) {
+    if (start < 0 || start > 6) {
+      return console.log('start date out of bounds');
+    }
+    const now = new Date; 
+    const year = now.getFullYear();
+    const month = now.getMonth();
+    const days = Object.keys(this.props.dates[year][month]);
+    let weeks = [];
+    let week = [];
+    let count = 0;
+    while (count < days.length + start) {
+      if (count < start) {
+        week.push(null);
+      } else {
+        week.push(days[count - start]);
+      }
+      if (week.length === 7) {
+        weeks.push(week);
+        week = [];
+      }
+      if (count === days.length + start - 1 && week.length < 7 && week.length) {
+        while (week.length < 7) {
+          week.push(null);
+        }
+        weeks.push(week);
+      }
+      count++;
+    }
+  }
+
 }
 
 export default Modal;
