22d931939cbab31b18b200d42991059e155bfac0
Francis Distor <francis.distor@gmail.com>
2018-12-31 12:24:34 -0800

    Refactored into function.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,24 +3,15 @@ import Summary from './Summary.jsx';
 import Price from './Price.jsx';
 import Book from './Book.jsx';
 
-class Navbar extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const date = new Date;
-
-    return (
-      <div>
-        <Summary type={this.props.type} location={this.props.location}
-          stars={this.props.stars} reviews={this.props.reviews}/>
-        <Price price={this.props.price}/>
-        <Book click={this.props.click}/>
-      </div>
-    );
-  }
-    
+const Navbar = ({ type, location, stars, reviews, price, click }) => {
+  return (
+    <div>
+      <Summary type={type} location={location}
+        stars={stars} reviews={reviews}/>
+      <Price price={price}/>
+      <Book click={click}/>
+    </div>
+  );
 }
 
 export default Navbar;
