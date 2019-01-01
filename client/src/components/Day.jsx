d7ff724c0e66c149009bdc5670588cc5664ca90d
Francis Distor <francis.distor@gmail.com>
2019-01-01 01:05:15 -0800

    Days before today are automatically unavailable.

--- client/src/components/Day.jsx
+++ client/src/components/Day.jsx
@@ -5,14 +5,21 @@ import getMonthDayYear from '../scripts/getMonthDayYear.js';
 const Day = ({ day, vacancy, select, year, month, checkin }) => {
   if (vacancy !== undefined) {
     let text = vacancy ? style.available : style.unavailable;
+    const today = new Date;
+    const nowMonth = today.getMonth();
+    const nowDay = today.getDate();
+    const nowYear = today.getFullYear();
+    const dayMonth = Number(month);
+    const dayDay = Number(day);
+    const dayYear = Number(year);
+    if (dayYear < nowYear || (dayMonth === nowMonth && dayYear === nowYear && dayDay < nowDay) || (dayYear === nowYear && dayMonth < nowMonth)) {
+      text = style.unavailable;
+    }
     if (checkin) {
       const check = getMonthDayYear(checkin);
       const inMonth = Number(check[0]);
       const inDay = Number(check[1]);
       const inYear = Number(check[2]);
-      const dayMonth = Number(month);
-      const dayDay = Number(day);
-      const dayYear = Number(year);
       if (dayYear < inYear || (dayMonth === inMonth && dayYear === inYear && dayDay < inDay) || (dayYear === inYear && dayMonth < inMonth)) {
         text = style.unavailable;
       }
