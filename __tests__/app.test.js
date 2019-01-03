cc67d0ffa5f6e3d1f34b28147a4f839aeb128429
Francis Distor <francis.distor@gmail.com>
2019-01-03 10:03:51 -0800

    Updated to use Availability and not App.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { shallow, mount, render } from 'enzyme';
 import toJson from 'enzyme-to-json';
-import App from '../client/src/components/App.jsx';
+import Availability from '../client/src/components/Availability.jsx';
 
-describe('App Render', () => {
+describe('Availability Render', () => {
   describe('render()', () => {
     test('Renders the top-level component', () => {
-      const wrapper =  shallow(<App />);
+      const wrapper =  shallow(<Availability />);
       const component = wrapper.shallow();
 
       expect(toJson(component)).toMatchSnapshot();
