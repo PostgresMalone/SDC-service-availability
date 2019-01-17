52b145f6b56186e833d2ec05150aea00b443d547
Aaron Deane <aaronmdeane@gmail.com>
2019-01-16 20:02:35 -0800

    Updated get route to create and release clients for pool query

--- __tests__/script.js
+++ __tests__/script.js
@@ -10,10 +10,10 @@ import { check, sleep } from 'k6';
 // };
 
 export default function () {
-  let id = Math.floor(Math.random() * (15000000 - 5000000)) + 5000000;
+  let id = Math.floor(Math.random() * (14000000 - 2000000)) + 2000000;
   let res = http.get(`http://127.0.0.1:1001/availabilities/${id}`);
   check(res, {
     'status was 200': r => r.status === 200
   });
-  sleep(1);
+  sleep(0.1);
 }
--- database/index.js
+++ database/index.js
@@ -3,14 +3,24 @@ const config = require('./config.js');
 
 const pool = new pg.Pool(config);
 
+pool.on('error', (err) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
+});
+
 const getAvailabilityById = (id, cb) => {
-  pool.query(`SELECT * FROM rooms, reservations WHERE rooms.roomid=reservations.roomid AND rooms.roomid=${id}`, (err, results) => {
-    if (err) {
-      cb(err);
-    } else {
-      cb(null, results.rows[0]);
-    }
-  });
+  pool.connect()
+    .then((client) => {
+      return client.query(`SELECT * FROM rooms, reservations WHERE rooms.roomid=reservations.roomid AND rooms.roomid=${id}`)
+        .then((res) => {
+          client.release();
+          cb(null, res.rows[0]);
+        })
+        .catch((err) => {
+          client.release();
+          cb(err.stack);
+        });
+    });
 };
 
 const modifyAvailabilityById = (id, options, cb) => {
