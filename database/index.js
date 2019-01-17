cb102bd98fe642d88338e3825e523c3800efbf79
Aaron Deane <aaronmdeane@gmail.com>
2019-01-16 16:22:30 -0800

    Completed get request for roomId

--- database/index.js
+++ database/index.js
@@ -1,32 +1,16 @@
-const mongoose = require('mongoose');
-const { Room, Reservation } = require('./models.js');
+const pg = require('pg');
+const config = require('./config.js');
 
-mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true }, err => {
-  if (err) { return console.log('Failed in connecting to MongoDB.'); }
-  console.log('Connected to MongoDB!');
-});
+const pool = new pg.Pool(config);
 
-const getVacancy = (listingId, callback) => {
-  Reservation.find({roomId: listingId})
-    .then(results => callback(results))
-    .catch(err => console.log('Error in getting from DB.', err));
-};
-
-const saveVacancy = (data) => {
-  Reservation.collection.drop({} , err => {
-    if (err) { return console.log('Error in dropping', err); }
-    Reservation.insertMany(data, err => {
-      if (err) { return console.log('Error in seeding.', err); }
-      mongoose.connection.close();
-    });
+const getAvailabilityById = (id, cb) => {
+  pool.query(`SELECT * FROM rooms, reservations WHERE rooms.roomid=reservations.roomid AND rooms.roomid=${id}`, (err, results) => {
+    if (err) {
+      cb(err);
+    } else {
+      cb(null, results.rows[0]);
+    }
   });
 };
 
-const updateVacancy = (listingId, updatedObj, callback) => { // delete this callback later
-  Reservation.update({roomId: listingId}, {availability: updatedObj}, (err, raw) => {
-    if (err) { return console.log('Err in database in saving, error ', raw); }
-    callback(null);
-  });
-};
-
-module.exports = { getVacancy, updateVacancy, saveVacancy };
+module.exports = getAvailabilityById;
--- server/index.js
+++ server/index.js
@@ -1,29 +1,42 @@
+// require('newrelic');
 const express = require('express');
 const bodyParser = require('body-parser');
-const db = require('../database/index');
-const compression = require('compression');
 const path = require('path');
+const db = require('../database/index');
+const getAvailabilityById = require('../database/index');
+
 const app = express();
 
-app.use(express.static(path.resolve(__dirname + '/../public')));
+app.use(express.static(path.resolve(__dirname, '/../public')));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(compression());
 
-app.get('/availabilities/:id', (req, res) => {
-  const listingId = req.params.id;
-  db.getVacancy(listingId, result => {
-    res.status(200).send(JSON.stringify(result));
+app.get('/availabilities', (req, res, next) => {
+  // TODO: Write function here to retrieve all roomId reservations
+});
+
+app.get('/availabilities/:id', (req, res, next) => {
+  const id = req.params.id;
+  getAvailabilityById(id, (err, result) => {
+    if (err) {
+      throw err;
+    } else {
+      res.status(200).json(result);
+    }
   });
 });
 
-app.put('/availabilities/:id', (req, res) => {
+app.put('/availabilities/:id', (req, res, next) => {
   const listingId = req.params.id;
-  const availability = req.body.avaiability;
-  db.updateVacancy(listingId, availability, () => { // delete this callback later
+  const reservation = req.body.avaiability;
+  db.updateVacancy(listingId, reservation, () => { // delete this callback later
     console.log('Success in updating!');
     res.status(204).end();
   })
+});
+
+app.post('/availabilities', (req, res, next) => {
+  // TODO: Write a function to insert a new room record and empty reservation object
 });
 
 const port = 1001;
